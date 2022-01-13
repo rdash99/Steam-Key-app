5d90f986db65f7b6a0383637b1fddde84ab50395
Rowan Dash <pokemonmaster9967@gmail.com>
2022-01-13 17:27:29 +0000

    Squashed commit of the following:
    commit 34f8beda6edb35913dafff2cd2114cceab2c3bcb
    Author: Rowan Dash <pokemonmaster9967@gmail.com>
    Date:   Thu Jan 13 17:26:51 2022 +0000
    
        Update pubspec.yaml
    
    commit ff437964035762a9fc67a49c85be423335ea8045
    Author: Rowan Dash <pokemonmaster9967@gmail.com>
    Date:   Thu Jan 13 16:05:25 2022 +0000
    
        Create image_page.dart
    
    commit 227213d987df03f5eab779985d528cb0f30998bd
    Author: Rowan Dash <pokemonmaster9967@gmail.com>
    Date:   Thu Jan 13 16:05:22 2022 +0000
    
        Create home_page.dart
    
    commit 2bf222d75a88c447644278fec3a0f85dc8a102ba
    Author: Rowan Dash <pokemonmaster9967@gmail.com>
    Date:   Thu Jan 13 16:05:18 2022 +0000
    
        Update tab_navigation_items.dart
    
    commit eaa99f61ee73e6745201413f05418aa4b6cd5872
    Author: Rowan Dash <pokemonmaster9967@gmail.com>
    Date:   Thu Jan 13 16:05:15 2022 +0000
    
        Update main.dart

---
+++ steam_key_app/lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class HomeRoute extends StatefulWidget {
+  @override
+  _HomeRouteState createState() => _HomeRouteState();
+}
+
+class _HomeRouteState extends State<HomeRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
