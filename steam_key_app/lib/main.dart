cffd2fe6541e040eef585f0982ddff857e082dd6
Rowan Dash <pokemonmaster9967@gmail.com>
2022-01-14 08:34:21 +0000

    Update main.dart

--- steam_key_app/lib/main.dart
+++ steam_key_app/lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:steam_key_app/page_navigation/tabs_page.dart';
 import 'package:steam_key_app/pages/home_page.dart';
 import 'package:steam_key_app/pages/image_page.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('steam_store_data');
   runApp(App());
 }
 
